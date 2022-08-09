7f64d21d04c42f4d6ac0ff2199a5936e6d791c75
Ygor Goulart <ygorgm95@gmail.com>
2022-08-09 14:45:36 -0300

    project 1 done

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const List = () => {
+// cria o elemento em uma variável
+// pega a variável 'people', que foi passada quando List foi chamado
+const List = ({people}) => {
   return (
     <>
-      <h2>list component</h2>
+      {/* mapeia e retorna cada elemento da variável 'people' */}
+      {people.map((person) => {
+
+        {/* desconstrói os dados da variável 'people'
+        afim de facilitar o código */}
+        const {id, name, age, image} = person;
+
+        return <article key={id} className="person">
+          <img src={image} alt={name} />
+          <div>
+            <h4>{name}</h4>
+            <p>{age} years</p>
+          </div>
+        </article>
+      })}
     </>
   );
 };
